6643a483843df67503c255621d956f1cdad1b05c
Andy L <andy.innersource@gmail.com>
2019-06-04 14:49:56 -0700

    support for executable jar

--- src/main/clojure/seazme/common/config.clj
+++ src/main/clojure/seazme/common/config.clj
@@ -1,10 +1,15 @@
 (ns seazme.common.config
-  (:require [clojure.edn :as edn]
-            [environ.core :refer [env]]))
+  (:require
+   [clojure.tools.logging :as log]
+   [clojure.java.io :as io]
+   [clojure.edn :as edn]
+   [environ.core :refer [env]]))
 
-(defn read-config []
-  (let [path (or (:config-file env)
-                 "config.edn")]
-    (edn/read-string (slurp path))))
+(defn read-config [env-name file-name]
+  (let [path (or (env-name env) file-name)]
+    (if (.exists (io/file path))
+      (edn/read-string (slurp path))
+      (do (log/warn "missing file:" file-name)
+          {}))))
 
-(def config (read-config))
+(def config (read-config :config-file "config.edn"))
